2df3f01db1664d8fb72e6b88c457b4d24d83c33e
yerim216 <bing_01@naver.com>
2025-04-02 17:25:56 +0900

    lamdba 압축 방식 수정

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,23 +1,28 @@
+data "archive_file" "main_lambda_archive_file" {
+  type        = "zip"
+  source_dir  = "${path.module}/../src"
+  output_path = "${path.module}/main_lambda_source_file.zip"
+}
+
 resource "aws_s3_object" "main_lambda_source_file" {
   bucket = aws_s3_bucket.main_lambda_source_bucket.id
   key    = "main_lambda_source_file.zip"
-  source = "${path.module}/main_lambda_source_file.zip"
-  etag   = filemd5("${path.module}/main_lambda_source_file.zip")
+  source = data.archive_file.main_lambda_archive_file.output_path
+  etag   = filemd5(data.archive_file.main_lambda_archive_file.output_path)
 }
 
 resource "aws_lambda_function" "main_handler" {
-  function_name = "main_handler"
-  handler       = "main_handler.main_handler"
-  role          = aws_iam_role.main_lambda.arn
+  function_name     = "main_handler"
+  handler           = "main_handler.main_handler"
+  role              = aws_iam_role.main_lambda.arn
 
   s3_bucket         = aws_s3_object.main_lambda_source_file.bucket
   s3_key            = aws_s3_object.main_lambda_source_file.key
-  source_code_hash  = filebase64sha256("${path.module}/main_lambda_source_file.zip")
+  source_code_hash  = data.archive_file.main_lambda_archive_file.output_base64sha256
 
-  runtime = "python3.10"
-  timeout = 10
+  runtime           = "python3.10"
+  timeout           = 10
+  publish           = true
 
-  publish = true
-
-  depends_on = [aws_s3_object.main_lambda_source_file] # 명시적 의존성 추가
+  depends_on        = [aws_s3_object.main_lambda_source_file]
 }
