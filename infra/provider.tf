ae92adba7e08b9a1247c80a398b7ab6dc22224b3
yerim216 <bing_01@naver.com>
2025-04-20 17:01:49 +0900

    fix provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -10,32 +10,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "ap-northeast-3"
     encrypt        = true
-    dynamodb_table = "lugeasy-terraform-state"
-  }
-}
-
-resource "aws_s3_bucket" "lugeasy_terraform_state_bucket" {
-  bucket = "jimjim-terraform-state-bucket-root"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "lugeasy_terraform_state" {
-  name           = "lugeasy-terraform-state"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
+    use_lockfile = true
   }
 }
