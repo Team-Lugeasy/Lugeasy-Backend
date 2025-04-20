c068ac04ef344fbd4e560d2c85d0cbf17be7663e
minu <gkqkehs0321@gmail.com>
2025-04-20 20:50:00 +0900

    delete acm

--- infra/data.tf
+++ infra/data.tf
@@ -1,7 +1,7 @@
-data "aws_route53_zone" "main" {
-  name         = "lugeasy.com"
-  private_zone = false
-}
-resource "aws_route53_zone" "main" {
-  name = "lugeasy.com"
-}
+# data "aws_route53_zone" "main" {
+#   name         = "lugeasy.com"
+#   private_zone = false
+# }
+# resource "aws_route53_zone" "main" {
+#   name = "lugeasy.com"
+# }
