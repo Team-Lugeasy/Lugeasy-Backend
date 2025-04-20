8dc9751a63c305fab33326aa3ddb7f22464ccf81
yerim216 <bing_01@naver.com>
2025-04-20 17:04:20 +0900

    fix route53.tf

--- infra/route53.tf
+++ infra/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "api_alias" {
-  zone_id = data.aws_route53_zone.lugeasy.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "api.lugeasy.com"
   type    = "A"
 
