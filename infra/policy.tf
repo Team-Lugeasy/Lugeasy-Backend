3acabdedcef5f15493dd152cb8569c9a44c95aa1
minu <gkqkehs0321@gmail.com>
2025-05-03 23:27:12 +0900

    [infra] lambda test

--- infra/policy.tf
+++ infra/policy.tf
@@ -40,3 +40,30 @@ resource "aws_iam_role_policy_attachment" "main_lambda_logging_attach" {
   policy_arn = aws_iam_policy.main_lambda_logging.arn
 }
 
+
+resource "aws_iam_policy" "main_lambda_ecr_access_policy" {
+  name        = "main_lambda_ecr_access_policy"
+  description = "IAM policy for Lambda to access ECR repository"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetDownloadToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetRepositoryPolicy",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage"
+        ],
+        Resource = "arn:aws:ecr:ap-northeast-1:448522292146:repository/lugeasy-main-ecr-repo"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "main_lambda_ecr_access_attachment" {
+  role       = aws_iam_role.main_lambda.name
+  policy_arn = aws_iam_policy.main_lambda_ecr_access_policy.arn
+}
