d57161b51bcfecc43b70df69493af6f3d25beefd
minu <gkqkehs0321@gmail.com>
2025-05-10 18:32:04 +0900

    [fix] root router

--- src/controller/root.py
+++ src/controller/root.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from service.root import RootService
 
-root_router = APIRouter(prefix="/")
+root_router = APIRouter(prefix="")
 
-@root_router.get("")
+@root_router.get("/")
 async def health_check(root_service: RootService = Depends(RootService)):
     response = root_service.health_check()
 
